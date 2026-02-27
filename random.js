4995c412cc59596d128edfb8d3ffb205005b1912
oldbeef33n13 <oldbeef33n13@gmail.com>
2026-02-27 22:42:23 +0800

    feat: extend random utilities

--- random.js
+++ random.js
@@ -1,4 +1,4 @@
-function randomInt(min, max) {
+function randomInt(min = 0, max = 100) {
   if (!Number.isInteger(min) || !Number.isInteger(max)) {
     throw new TypeError('min and max must be integers');
   }
@@ -10,4 +10,16 @@ function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-module.exports = { randomInt };
+function randomFloat(min = 0, max = 1) {
+  if (typeof min !== 'number' || typeof max !== 'number') {
+    throw new TypeError('min and max must be numbers');
+  }
+
+  if (min > max) {
+    throw new RangeError('min must be less than or equal to max');
+  }
+
+  return Math.random() * (max - min) + min;
+}
+
+module.exports = { randomInt, randomFloat };
